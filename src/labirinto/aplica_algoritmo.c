24b9c7391de9ebe114439e1ff4a834c39d3d90ef
Rener-Pontes <renercpontes@gmail.com>
2023-10-29 00:09:18 -0300

    Implementado as funções que irão aplicar o algoritmo escolhido.

--- src/labirinto/aplica_algoritmo.c
+++ src/labirinto/aplica_algoritmo.c
@@ -1,23 +1,36 @@
+#include <stdio.h>
+
 #include "lab.h"
 #include "../libs/list/list.h"
 
-extern vertex pa_dfs[1000];
+extern int num_bfs[1000];
+extern int post_dfs[1000];
 extern vertex pa_bfs[1000];
+extern vertex pa_dfs[1000];
 
 void aplica_bfs(Graph labirinto, vertex inicial, vertex final) {
-	GRAPHdfs(labirinto);
+	GRAPHbfs(labirinto, inicial);
 
-	for (vertex v = final; v != inicial; v = pa_dfs[v])
-		printf("[[%02d]=>", v);
+	if (num_bfs[final] == -1) {
+		printf("Não é acessível.\n");
+		return;
+	}
 
-	printf("[[%02d]=>", inicial);
+	for (vertex v = final; v != inicial && v != -1; v = pa_bfs[v])
+		printf("[[%02d]=>|", v);
+	printf("[[%02d]\n", inicial);
 }
 
 void aplica_dfs(Graph labirinto, vertex inicial, vertex final) {
-	GRAPHbfs(labirinto, inicial);
+	GRAPHdfs(labirinto, inicial);
 
-	for (vertex v = final; v != inicial; v = pa_bfs[v])
-		printf("[[%02d]=>", v);
-	
-	printf("[[%02d]=>", inicial);
+	if (post_dfs[inicial] < post_dfs[final]) {
+		printf("Não é acessível.\n");
+		return;		
+	}
+
+	for (vertex v = final; v != inicial && v != -1; v = pa_dfs[v])
+		printf("[[%02d]=>|", v);
+	printf("[[%02d]\n", inicial);
+
 }
